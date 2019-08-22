0bb2af0bed413c7586f98025556544ca422454e0
Michael Bastos <bastosmichael@gmail.com>
2019-08-22 15:28:25 -0500

    adding testle-auth ;

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'trestle'
+gem 'trestle-auth'
 gem 'ahoy_matey'
 
 group :development, :test do
---
+++ spec/factories/administrators.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :administrator do
+    email { "MyString" }
+    password_digest { "MyString" }
+    first_name { "MyString" }
+    last_name { "MyString" }
+    remember_token { "MyString" }
+    remember_token_expires_at { "2019-08-22 15:16:08" }
+  end
+end
